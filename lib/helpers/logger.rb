14e4e56340041e5ab6b16c8cb7f9e88fdc94b23e
Rick Banks <namaste@rawrnet.net>
2015-07-01 19:10:25 -0500

    Added function for adding the log to a file for debugging purposes

--- lib/helpers/logger.rb
+++ lib/helpers/logger.rb
@@ -1,16 +1,26 @@
 require 'highline'
 require_relative 'timestamp'
+require_relative 'file_handler'
 
 module Cinch
   module Helpers
-    
+
     def log_message(level, message, plugin_name="Core", timer="no")
       time = timestamp
       name = "| <%= color('#{plugin_name}', BOLD, UNDERLINE, GREEN) %> |"
-      indicator = "<%= color('--', BOLD, GREEN) %>" if level == "message"
-      indicator = "<%= color('**', BOLD, YELLOW) %>" if level == "warn"
-      indicator = "<%= color('!!', BOLD, RED) %>" if level == "error"
-      @logMessage = "#{time} #{name} #{indicator} #{message}"
+      if level == "message"
+        indicator  = "<%= color('--', BOLD, GREEN) %>"
+        logMessage = "<%= color('#{time} #{name} #{indicator} #{message}', GREEN) %>"
+      end
+      if level == "warn"
+        indicator  = "<%= color('**', BOLD, YELLOW) %>"
+        logMessage = "<%= color('#{time} #{name} #{indicator} #{message}', YELLOW) %>"
+      end
+      if level == "error"
+        indicator  = "<%= color('!!', BOLD, RED) %>"
+        logMessage = "<%= color('#{time} #{name} #{indicator} #{message}', RED) %>"
+        time="yes"
+      end
       say("#{@logMessage}")
       sleep(2) if timer == "yes"
     end
