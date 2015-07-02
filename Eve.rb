85fd6a3ba85e1044834490b672d068b81ddbff2d
Rick Banks <namaste@rawrnet.net>
2015-07-01 19:09:56 -0500

    Version bump to 7.0-alpha2.3.0 - Added programming that correlates with the changes made to first_run.rb

--- Eve.rb
+++ Eve.rb
@@ -5,7 +5,7 @@ require_relative 'lib/utils/config_checks'
 require_relative 'lib/helpers/file_handler'
 require_relative 'lib/helpers/to_bool'
 
-$eve_version = "7.0alpha2.2.0"
+$eve_version = "7.0alpha2.3.0"
 
 # Find out of there is a config file, if
 # there is a config file then it pulls
@@ -18,12 +18,16 @@ if $settings_file.nil?
   server_port = "6667"
   server_ssl  = "false"
   ssl_verify  = "false"
+  server_user = "Eve"
+  server_pass = "password"
   botnick     = "Eve"
 else
   irc_server  = $settings_file['irc_server'].to_s
   server_port = $settings_file['server_port'].to_s
   server_ssl  = $settings_file['server_ssl']
   ssl_verify  = $settings_file['ssl_verify']
+  server_user = $settings_file['bnc_username']
+  server_pass = $settings_file['bnc_password']
   botnick     = $settings_file['nick']
 end
 
@@ -31,6 +35,8 @@ bot = Cinch::Bot.new do
   configure do |c|
     c.server          = "#{irc_server}"
     c.port            = "#{server_port}"
+    c.password        = "#{server_pass}"
+    c.user            = "#{server_user}"
     c.ssl.use         = server_ssl.to_bool
     c.ssl.verify      = ssl_verify.to_bool
     c.channels        = [
